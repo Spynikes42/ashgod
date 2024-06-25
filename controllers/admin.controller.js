fe8386088b70b453d67539cb302a4f4f9d9595c8
tejas_sonawane <tejassonawane4242@gmail.com>
2024-06-25 16:19:50 +0530

    third

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -4,7 +4,7 @@ const { upload } = require("../utils/upload")
 const cloudinary = require("cloudinary").v2
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 })
